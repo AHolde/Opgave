66cd864c8cb2e44f17d4881423083c74d974d904
AHolde <anton@holde.net>
2023-09-20 12:08:32 +0200

    Nye mapper

--- src/server.js
+++ src/server.js
@@ -1,19 +1,56 @@
 const express = require("express");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+// const cors = require("cors");
 
 const app = express();
+const port = 3000
 
-const customerRoute = require("./routes/customer");
+// app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(cors());
-app.use(express.json());
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const host = 'localhost';
+const path = require('path')
 
-app.use("/customer", customerRoute);
+app.use(express.static(__dirname + '../client'));
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/home.html'));
+  
 });
 
-app.listen(3000, () => {
-  console.log("Server open on port 3000");
+app.get('/home.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/home.js'));
+  
 });
+
+app.get('/global.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/global.css'));
+  
+});
+
+// app.get('/styles.css', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/styles/global.css'));
+  
+// });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+  socket.on('user joined', username => {
+    console.log(username + " joined the chat")
+    io.emit('chat message', username + " joined the chat");
+  });
+});
+
+http.listen(port, host, () => {
+  console.log(`Socket.IO server running at http://${host}:${port}/`);
+});
+
+// server = app.listen(port, () => {
+//   console.log(`Server open on port ${port}`);
+// });
+ 
